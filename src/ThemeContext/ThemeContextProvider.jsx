43d2220467865fda38caa8ce9f0b29bd011396a1
mYashavanth <yashavantham143@gmail.com>
2023-12-11 21:29:15 +0530

    final Portfolio

--- src/ThemeContext/ThemeContextProvider.jsx
+++ src/ThemeContext/ThemeContextProvider.jsx
@@ -3,7 +3,7 @@ import React, { createContext, useState } from "react";
 export const ThemeContext = createContext()
 
 export default function ThemeContextProvider({children}){
-    const [isDark,setIsDark] = useState(false)
+    const [isDark,setIsDark] = useState(true)
 
     return(
         <ThemeContext.Provider value={{isDark,setIsDark}}>
