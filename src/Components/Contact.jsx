f3d3eab8d289077193bc3192cc5aa5d540c5e7b1
mYashavanth <yashavantham143@gmail.com>
2023-12-13 23:02:08 +0530

    added statistics to portfolio

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -197,9 +197,7 @@ export default function Contact() {
             <Box display={"flex"} flexDirection={"column"} gap={2} mb={4}>
               <Box display={"flex"} alignItems={"center"} gap={2}>
                 <TfiEmail />
-                <Text as={"a"} href="mailto:yashavantham143@gmail.com">
-                  yashavantham143@gmail.com
-                </Text>
+                <Text>yashavantham143@gmail.com</Text>
               </Box>
               <Box display={"flex"} alignItems={"center"} gap={2}>
                 <ImMobile />
